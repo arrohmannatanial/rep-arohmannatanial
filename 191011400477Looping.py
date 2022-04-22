5765a6d919722678036460bcc17f2eac152c88f2
arrohmannatanial <104188662+arrohmannatanial@users.noreply.github.com>
2022-04-22 14:13:33 +0700

    Add files via upload

---
+++ 191011400477Looping.py
@@ -0,0 +1,16 @@
+#Contoh pengulangan for sederhana
+angka = [1,2,3,4,5]
+for x in angka:
+    print(x)
+
+#Contoh pengulangan for
+buah = ["nanas", "apel", "jeruk"]
+for makanan in buah:
+    print ("Saya suka makan", makanan)
+#Contoh penggunaan While Loop
+count = 0
+while (count < 9):
+    print ("The count is: ", count)
+    count = count + 1
+
+print ("Good bye!")
